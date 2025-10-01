80d2e13c1eadb8a09023e18f3d500ab7372e646d
HHZKYO <1692189552@qq.com>
2025-10-01 16:48:06 +0800

    引入vant和移动端适配

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import { Button } from 'vant'
+</script>
 
 <template>
-  <div>app</div>
+  <div>
+    app
+    <!-- <Button type="primary">按钮</Button> -->
+    <van-button>按钮</van-button>
+  </div>
 </template>
 
 <style scoped></style>
