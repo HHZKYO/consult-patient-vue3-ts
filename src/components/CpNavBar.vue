1c2dcf7f0fd07ccad157c9d0593ae9540fb3a8b1
HHZKYO <1692189552@qq.com>
2025-12-11 16:25:51 +0800

    cp-nav-bar功能标题和右侧文字与右侧文字自定义事件

--- src/components/CpNavBar.vue
+++ src/components/CpNavBar.vue
@@ -1,6 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  defineProps<{
+    title?: string
+    rightText?: string
+  }>()
+
+  // const emit = defineEmits(['click-right'])
+  const emit = defineEmits<{
+    (e: 'click-right'): void
+  }>()
+
+  const onClickRight = () => {
+    emit('click-right')
+  }
+</script>
 <template>
-  <van-nav-bar fixed title="登录" left-arrow right-text="注册" />
+  <van-nav-bar @click-right="onClickRight" fixed :title="title" left-arrow :right-text="rightText" />
 </template>
 <style lang="scss" scoped>
   :deep() {
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+  const fn = () => {
+    console.log('fn')
+  }
 </script>
 <template>
   <div class="login-page">
-    <CpNavBar />
+    <CpNavBar @click-right="fn" title="登录" right-text="注册" />
     <h1>Login</h1>
     <van-button type="primary">登录</van-button>
   </div>
