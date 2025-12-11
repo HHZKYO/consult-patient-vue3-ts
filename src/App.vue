8796eafa2deadcab4415de2b1f4398dcbded6101
HHZKYO <1692189552@qq.com>
2025-12-11 17:43:28 +0800

    登录页面布局和清理代码

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="app">
-    <!-- 一级路由出口 -->
-     <RouterLink to="/login">登录</RouterLink>
-    <RouterView />
-  </div>
+  <!-- 一级路由出口 -->
+  <RouterView />
 </template>
 
 <style lang="scss" scoped></style>
