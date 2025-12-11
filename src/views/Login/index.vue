ddb2ee5b454d9fcd7fa8d7e66597b20781914f23
HHZKYO <1692189552@qq.com>
2025-12-11 11:54:02 +0800

    配置vant组件的自动注册

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,6 +4,7 @@
 <template>
   <div class="login-page">
     <h1>Login</h1>
+    <van-button type="primary">登录</van-button>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// 配置组件自动注册的插件
+import Components from 'unplugin-vue-components/vite'
+// 配置组件自动注册的解析器
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/hm',
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    // 样式重复引入了，类型声明文件重复了
+    Components({
+      dts: false,
+      resolvers: [VantResolver({ importStyle: false })]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
