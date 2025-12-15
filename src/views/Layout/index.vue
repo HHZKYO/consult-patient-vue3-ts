ee2f7c4e5b84c5b33f14c41bc1ec707f3df1d112
HHZKYO <1692189552@qq.com>
2025-12-15 15:36:39 +0800

    组件与路由配置

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout-page">
+    <!-- 二级路由出口 -->
+     <router-view></router-view>
+     <div>tab-bar</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
