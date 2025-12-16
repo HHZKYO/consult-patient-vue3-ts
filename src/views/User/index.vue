91344b94d615992e162190d17cf294514f65165f
HHZKYO <1692189552@qq.com>
2025-12-16 09:30:41 +0800

    个人中心-用户信息类型

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { UserInfo } from '@/types/user';
+import { ref } from 'vue';
+
+const userInfo = ref<UserInfo>()
+console.log(userInfo.value?.orderInfo.receivedNumber)
+</script>
 
 <template>
   <div class="user-page">
