303051872c58ac2766951e79facd0e86c53c2f2c
HHZKYO <1692189552@qq.com>
2025-12-20 15:09:21 +0800

    适配滑动宽度-采用VueUse

--- src/views/Home/components/FollowDoctor.vue
+++ src/views/Home/components/FollowDoctor.vue
@@ -1,6 +1,22 @@
 <script setup lang="ts">
 import DoctorCard from './DoctorCard.vue';
+import { useWindowSize } from '@vueuse/core'
 
+const { width } = useWindowSize()
+
+// 使用原生js来实现宽度适配
+// import { onMounted, onUnmounted, ref } from 'vue';
+// const width = ref(0)
+// const setWidth = () => {
+//   width.value = window.innerWidth
+// }
+// onMounted(() => {
+//   setWidth()
+//   window.addEventListener('resize', setWidth)
+// })
+// onUnmounted(() => {
+//   window.removeEventListener('resize', setWidth)
+// })
 </script>
 
 <template>
@@ -14,7 +30,7 @@ import DoctorCard from './DoctorCard.vue';
       <van-swipe
         :loop="false"
         :show-indicators="false"
-        :width="150"
+        :width="( 150 / 375 ) * width"
       >
         <van-swipe-item v-for="i in 5" :key="i">
           <doctor-card></doctor-card>
