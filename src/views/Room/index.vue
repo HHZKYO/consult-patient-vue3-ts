397ac98b13597940189ade02f040286529ffe48c
HHZKYO <1692189552@qq.com>
2026-01-05 11:56:09 +0800

    问诊室聊天（一）-组件拆分页面布局

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
+import RoomStatus from './components/RoomStatus.vue'
+import RoomAction from './components/RoomAction.vue'
+import RoomMessage from './components/RoomMessage.vue';
 </script>
 
 <template>
-  <h1>111</h1>
+  <div class="room-page">
+    <cp-nav-bar title="问诊室"></cp-nav-bar>
+    <!-- 状态栏 -->
+    <room-status></room-status>
+    <!-- 消息栏 -->
+    <room-message></room-message>
+    <!-- 操作栏 -->
+    <room-action></room-action>
+  </div>
 </template>
 
-<style  lang="scss" scoped></style>
+<style  lang="scss" scoped>
+.room-page {
+  padding-top: 90px;
+  padding-bottom: 60px;
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-color: var(--cp-bg);
+  .van-pull-refresh {
+    width: 100%;
+    min-height: calc(100vh - 150px);
+  }
+}
+</style>
